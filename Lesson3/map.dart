fa28f3648fe7bdec6678bff2a4574a945efba61f
Ruveyda <ruveydakisla34@gmail.com>
2022-10-22 13:31:54 +0300

    map practices added  |3.4

--- Lesson3/map.dart
+++ Lesson3/map.dart
@@ -7,23 +7,39 @@ void main() {
     'Mustafa': 18
   }; // Key :value
 
-  // for (var item in users.values) {
-  //   print(item);
-  // }
+  for (var item in users.values) {
+    print(item);
+  }
 
-  // for (int i = 0; i < users.length; i++) {
-  //   print(users.values.elementAt(i));
-  // }
+  for (int i = 0; i < users.length; i++) {
+    print(users.values.elementAt(i));
+  }
 
   //Bir bankanın müşterilerinin birden fazla hesabı olabilir.
-  
 
   Map<String, List<int>> Bank = {
-    'Ruveyda': [100, 200, 300],
+    'Ruveyda': [100, 200, 300, 1000],
     'Salih': [20, 500],
-    'Mustafa': [540, 740, 100, 550]
+    'Mustafa': [0, 740, 100, 50]
   };
 
   print(Bank);
+  Bank['Ayşe'] = [10, 500, 440];
+  print(Bank);
+  for (var item in Bank.keys) {
+    for (var money in Bank[item]!) {
+      if (money > 500) print('krediniz hazır $item');
+    }
+  }
 
+//Müşterilerin hesaplarındaki toplam parayı bulalım.
+//Bank[item]! --> musterinin hesapları
+  for (var item in Bank.keys) {
+    int toplam = 0;
+    for (var money in Bank[item]!) {
+      toplam+=money;
+
+    }
+    print('Toplam paranız $toplam sayın $item ');
+  }
 }
