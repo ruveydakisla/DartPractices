0ac069c25c7522474e57977d8169f7c9fe6bec4a
Ruveyda <ruveydakisla34@gmail.com>
2022-10-23 17:13:13 +0300

    callback practice added | 6.4

---
+++ Lesson6/callback.dart
@@ -0,0 +1,16 @@
+void main(List<String> args) {
+  final user = User(money: 5);
+  user.calculateMoney((int result) {
+    print(user.money);
+  });
+}
+
+class User {
+  int money;
+  User({required this.money});
+
+  void calculateMoney(void Function(int data) onComplete) {
+    money += 5;
+    onComplete(money);
+  }
+}
